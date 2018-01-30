1fc3ffe887ec35aabef5e94b6b5b84d536b65da7
hao <17802118921>
2018-01-30 16:44:05 +0800

    fragment的resuolt调整

--- library/src/main/java/com/example/library/CropUtils.java
+++ library/src/main/java/com/example/library/CropUtils.java
@@ -1,13 +1,13 @@
 package com.example.library;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
